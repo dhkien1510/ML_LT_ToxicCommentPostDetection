83ec0de6194b5b41cef4efe8c424e447316819ac
dhkien1510 <dhkien1510@users.noreply.github.com>
2025-12-28 12:21:59 +0700

    Update scrapper

--- data_scrapper/scrape/facebook-scraper/parase.py
+++ data_scrapper/scrape/facebook-scraper/parase.py
@@ -1,7 +1,7 @@
 import csv
 
-input_file = "output2.txt"
-output_file = "comments16.csv"
+input_file = "output1.txt"
+output_file = "comments17.csv"
 
 rows = []
 with open(input_file, "r", encoding="utf-8") as f:
@@ -14,20 +14,21 @@ k = 0
 while k < len(lines):
   
     new_line += lines[k] 
-    if (lines[k] == "Reply"):
+    if (lines[k] == "Trả lời"):
         new_lines.append(new_line)
         new_line = ""
     else:
         new_line += ">"
     k += 1
 
-print(len(lines))
+
 lines = new_lines
+print(len(lines))
 
 filter_line = []
 for i in lines:
     seg = i.split(">")
-    seg = [j.strip() for j in seg if j.strip() not in [".", "", "Top fan", "Hay Hóng Hớt", "·"]]
+    seg = [j.strip() for j in seg if j.strip() not in [".", "", "Top fan", "Hay Hóng Hớt", "·", "Chia sẻ"]]
     if seg[0] == 'Edited':
         seg = seg[1:]
     filter_line.append(seg)
@@ -37,5 +38,6 @@ lines = filter_line
 with open(output_file, "w", newline="", encoding="utf-8") as csvfile:
     csvfile.write("name,comment\n")
     for i in lines:
+
         csvfile.write(f'{i[0]}, "{i[1]}"\n')
 
